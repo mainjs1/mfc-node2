7a9abc899e3d5255ff54822a967af75b3c6432be
horacio9a <horacio9a@gmail.com>
2017-07-06 16:30:15 +0200

    Update nm.bat

--- nm.bat
+++ nm.bat
@@ -3,7 +3,7 @@ SETLOCAL EnableDelayedExpansion
 :START
 COLOR 0F
 ECHO.
-SET /P MODE=EXIT(3) - ShowUp(2) - Chaturbate(1) - MyFreeCams(0)(ENTER): 
+SET /P MODE=Exit(3) - ShowUp(2) - ChaturBate(1) - MyFreeCams(0)(ENTER)(%MODE%): 
 ECHO.
 IF "%MODE%"=="" GOTO MFC
 IF "%MODE%"=="0" GOTO MFC
@@ -11,28 +11,28 @@ IF "%MODE%"=="1" GOTO CB
 IF "%MODE%"=="2" GOTO SU
 IF "%MODE%"=="3" GOTO EXIT
 :MFC
-CLS && ECHO ########################################
-ECHO ###  MFC Node Recorder by horacio9a  ###
-ECHO ########################################
-cd/
+CLS && ECHO ######################################
+ECHO ### MFC Node Recorder by horacio9a ###
+ECHO ######################################
+cd C:\
 cd -nm-mfc
 start node main.js
 ECHO.
 GOTO START
 :CB
-CLS && ECHO #######################################
-ECHO ###  CB Node Recorder by horacio9a  ###
-ECHO #######################################
-cd/
+CLS && ECHO #####################################
+ECHO ### CB Node Recorder by horacio9a ###
+ECHO #####################################
+cd C:\
 cd -nm-cb
 start node main.js
 ECHO.
 GOTO START
 :SU
-CLS && ECHO #######################################
-ECHO ###  SU Node Recorder by horacio9a  ###
-ECHO #######################################
-cd/
+CLS && ECHO #####################################
+ECHO ### SU Node Recorder by horacio9a ###
+ECHO #####################################
+cd C:\
 cd -nm-su
 start node main.js
 ECHO.
